c0d0a632f5749e92ac5c1af2b8119ab7c856d8a7
Adam Butler <adam@littlebigmedia.co.uk>
2015-08-07 09:50:47 +0100

    Add gemrc

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.irbrc ~/.irbrc  
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc  
+ln -s ~/.dotfiles/.gemrc ~/.gemrc  
