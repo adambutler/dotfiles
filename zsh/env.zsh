6d31f8bd4ca5769097ec496bdaa7fb061e150321
Adam Butler <adam@lab.io>
2016-07-04 14:05:20 +0100

    Add Ansible variables

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -9,3 +9,9 @@ eval "$(/Users/adambutler/.simple/bin/simple init -)"
 
 export NVM_DIR=~/.nvm
 source $(brew --prefix nvm)/nvm.sh
+
+### Ansible
+if [[ -a ~/.vault_pass ]]; then
+  export ANSIBLE_VAULT_PASSWORD_FILE=~/.vault_pass
+fi
+export ANSIBLE_HOST_KEY_CHECKING=False
