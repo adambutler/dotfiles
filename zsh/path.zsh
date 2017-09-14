688af460640ebe8ef653e01674129ddef3f77133
Adam Butler <adam@lab.io>
2017-09-14 09:52:24 +0100

    Add yarn path HOME path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -5,13 +5,18 @@ export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin
 export PATH=/usr/local/heroku/bin:$PATH
 
 ### Android
-export ANDROID_HOME=~/Library/Android/sdk
-export PATH=${PATH}:${ANDROID_HOME}/tools
-export PATH=${PATH}:${ANDROID_HOME}/platform-tools
+# export ANDROID_HOME=~/Library/Android/sdk
+# export PATH=${PATH}:${ANDROID_HOME}/tools
+# export PATH=${PATH}:${ANDROID_HOME}/platform-tools
 
 ## #rbenv
 export PATH=$HOME/.rbenv/bin:$PATH
 
 ### yarn
-export PATH=$HOME/.yarn/bin:$PATH
-export PATH=$HOME/.yarn-config/global/node_modules/.bin:$PATH
+export PATH="$PATH:`yarn global bin`"
+export PATH="$HOME/.yarn/bin:$PATH"
+
+### Swift toolchain
+export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
+
+#
