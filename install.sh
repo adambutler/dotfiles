a288ed00e072bd389c5426941e2faf7aaf087a95
Adam Butler <adam@labfoo.dev>
2022-03-30 09:31:28 +0100

    Remove rbenv and nvm as I'll be using adsf going forward

--- install.sh
+++ install.sh
@@ -5,4 +5,3 @@ ln -s ~/.dotfiles/.irbrc ~/.irbrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gemrc ~/.gemrc
-ln -s ~/.dotfiles/atom ~/.atom
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -2,15 +2,6 @@
 export BUNDLER_EDITOR=code
 export EDITOR="code -w"
 
-### RB Env
-eval "$(rbenv init --no-rehash -)"
-(rbenv rehash &) 2> /dev/null
-
-### NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-
 ### Ansible
 if [[ -a ~/.vault_pass ]]; then
   export ANSIBLE_VAULT_PASSWORD_FILE=~/.vault_pass
