6eca0ff43ffdd7507ae169c2fec459b53f666ecc
Adam Butler <adam@labfoo.dev>
2022-03-30 09:21:33 +0100

    Change ZSH plugin osx to macos

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -2,7 +2,7 @@ ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="crunch"
 
 COMPLETION_WAITING_DOTS="true"
-plugins=(git autojump brew bundler git-extras git-hubflow osx)
+plugins=(git autojump brew bundler git-extras git-hubflow macos)
 
 source $ZSH/oh-my-zsh.sh
 
